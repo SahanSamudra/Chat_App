104c1d49f2942ad5b8b3e975c4cae79c57ea9084
Sahan Samudra <sahansamudra123@gmail.com>
2023-07-13 18:54:35 +0530

    Create Login Form.

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,4 +1,42 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class LoginFormController {
+
+
+
+    private static String username;
+    public AnchorPane LoginContext;
+    public JFXTextField txtUserName;
+
+    public void btnJoinOnAction(ActionEvent event) {
+        username = txtUserName.getText();
+        try {
+            loadClient();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadClient() throws IOException {
+        URL resource = getClass().getResource("/view/client_form.fxml");
+        assert resource != null;
+        Parent load = FXMLLoader.load(resource);
+        LoginContext.getChildren().clear();
+        LoginContext.getChildren().add(load);
+    }
+
+    public static String getUserName(){
+        return username;
+    }
+
+
 }
